3c0f821be8e4495884e2d825a93b6632171898c4
matteo <s223012@studenti.polito.it>
2018-10-25 11:56:53 +0200

    trial and error

--- lab1/ex3_v2.py
+++ lab1/ex3_v2.py
@@ -6,14 +6,14 @@ class DiscoManager(object):
         self.data = data
     def search_by_artist(self, artist_name):
         try:
-            for artist_name in data["album_list"]["artist"]:
+            if artist_name in data.album_list.artist:
                 print "the artist's disc:  ", data["album_list"]
         except Exception, e:
-            print "not found the ", artist_name , " artist"
+            print "not found the ", artist_name, " artist"
 
 if __name__ == "__main__":
     script, filename = argv
-    print "this is ur script and file: ", script , filename
+    print "this is ur script and file: ", script, filename
     # loading file
     with open(filename) as fp:
         data = json.load(fp)
@@ -23,11 +23,10 @@ if __name__ == "__main__":
         choose a command by typing it:
         1)search_by_artist
         2)search_by_title
-        3)
         ''')
         op = DiscoManager()
         if operation == "search_by_artist":
-            artist=raw_input("give me artist name: ")
+            artist = raw_input("give me artist name: ")
             op.search_by_artist(artist)
         else:
             break
