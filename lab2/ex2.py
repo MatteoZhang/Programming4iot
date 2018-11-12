9025e8e263f983c62bfd29d11dbea6c70c662e97
Ninja <s223012@studenti.polito.it>
2018-11-12 23:28:41 +0100

    ex2

---
+++ lab2/ex2.py
@@ -0,0 +1,51 @@
+import cherrypy
+from asset.calculator import OP
+import json
+
+
+class WebService(object):
+    exposed = True
+
+    class HelloWorld(object):
+        @cherrypy.expose
+        def index(self):
+            return "Hello world!"
+
+    def GET(self, *uri, **params):
+        a = float(uri[1])
+        b = float(uri[2])
+        operation = str(uri[0])
+        dictionary = {}
+        result = OP()
+        if operation == 'add':
+            r = result.Add(a, b)
+        elif operation == 'sub':
+            r = result.Sub(a, b)
+        elif operation == 'mul':
+            r = result.Mul(a, b)
+        elif operation == 'div':
+            r = result.Div(a, b)
+        else:
+            print "wrong url"
+        dictionary['result'] = r
+        dictionary['op1'] = a
+        dictionary['op2'] = b
+        dictionary['operation'] = operation
+        print dictionary
+        print "uri: %s" % (str(uri))
+        return json.dumps(dictionary)
+
+if __name__=='__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+    cherrypy.tree.mount(WebService(), '/string', conf)
+    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    cherrypy.config.update({'server.socket_port': 9090})
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+## usare http://localhost:9090/string/add/0/1
