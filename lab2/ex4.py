3440e5f03de2db19fac80cd606e656a38cd4ed8d
Ninja <s223012@studenti.polito.it>
2018-11-15 23:54:41 +0100

    ex2

--- lab2/ex4.py
+++ lab2/ex4.py
@@ -1,4 +1,5 @@
 import cherrypy
+import json
 
 class WebService(object):
     exposed = True
@@ -6,10 +7,20 @@ class WebService(object):
     def GET(self, *uri, **param):
         with open("freeboard/index.html") as fp:
             index = fp.read()
+        print uri, param
         return index
+
+    def POST(self, *uri, **params):
+        dash = json.loads(params["json_string"])  # Load json object
+        with open("./freeboard/dashboard/dashboard.json", "w") as f:
+            json.dump(dash, f, indent=2)  # Write json to file
+        print uri, params
 
 if __name__=='__main__':
     cherrypy.tree.mount(WebService(), '/', config='server.conf')
     cherrypy.config.update('server.conf')
     cherrypy.engine.start()
     cherrypy.engine.block()
+
+##C:/Users/Ninja/PycharmProjects/Programming4iot/lab2/freeboard a casa
+##C:/Users/matte/Documents/PycharmProjects/Programming4iot/lab2/freeboard laptop
