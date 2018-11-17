14fa02d423b1546b9d23aaa48ee22ddbf35094b6
Ninja <s223012@studenti.polito.it>
2018-11-17 22:32:11 +0100

    Discography

--- lab3/ex2client.py
+++ lab3/ex2client.py
@@ -9,14 +9,15 @@ if __name__=="__main__":
         "print_all",
         "exit"]
     while True:
-        print """\nDiscography manager:
-        --search_by_artist
-        --search_by_title
-        --search_by_pub_year
-        --search_by_tot_tracks
-        --insert_new
-        --print_all 
-        --exit"""
+        print """\nDiscography manager u can do the following 
+        operations on the file discography:
+        search_by_artist
+        search_by_title
+        search_by_pub_year
+        search_by_tot_tracks
+        insert_new
+        print_all 
+        exit"""
         operation = raw_input("choose operation: ")
         while operation not in list:
             operation = raw_input("choose operation (this time choose it right): ")
