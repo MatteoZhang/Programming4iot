c516e30729339a607abfb381e30aa6ba7315d2c9
matteo.zh <37191615+MatteoZhang@users.noreply.github.com>
2018-11-26 12:46:41 +0100

    FIX

--- lab3/ex3_server.py
+++ lab3/ex3_server.py
@@ -5,8 +5,8 @@ import requests
 LINK = 'https://www.bicing.cat/availability_map/getJsonObject'
 
 class MyOrder(object):
-    def order_by_slots(self, dictionary, n=10, rev=True):
-        dictionary_sorted = sorted(dictionary, key=lambda entry: int(entry['slots']), reverse=rev)
+    def order_by_par(self, dictionary, n=10, rev=True, par):
+        dictionary_sorted = sorted(dictionary, key=lambda entry: int(entry[par]), reverse=rev)
         return dictionary_sorted[1:n]
 
 class WebServer(object):
@@ -16,15 +16,58 @@ class WebServer(object):
         r = requests.get(LINK)
         print "received uri: ", uri, " ; params: ", params
         if uri[0] == 'slots':
-            Order = MyOrder()
             try:
+              	par = uri[0]
                 N = int(params['N'])
                 reversion = bool(params['reversion'])
-                obj_ordered = Order.order_by_slots(r.json(), N, reversion)
-                print "checkpoint"
+                Order = MyOrder()
+                obj_ordered = Order.order_by_par(r.json(), N, reversion, par)
                 return json.dumps(obj_ordered)
             except Exception, e:
                 print "error: ", e
+		if uri[0] == 'bikes':
+        	try:
+              	par = uri[0]
+                N = int(params['N'])
+                reversion = bool(params['reversion'])
+                Order = MyOrder()
+                obj_ordered = Order.order_by_par(r.json(), N, reversion, par)
+                return json.dumps(obj_ordered)
+            except Exception, e:
+                print "error: ", e
+        # matches = [x for x in lst if fulfills_some_condition(x)] 
+        # https://stackoverflow.com/questions/9542738/python-find-in-list
+		if uri[0] == 'zip':
+          	try:
+              	zipcode = params['zipcode']
+            except Exception, e:
+              	print "error: ", e 
+              	print "zipcode must be specified try /zip?zipcode=08025"
+            matches = r.json()
+            matches = [x for x in lst if x['zip'] == zipcode] 
+        	return json.dumps(matches)
+        if uri[0] == 'ELECTRIC_BIKE':
+          	try:
+              	N = int(params['N'])
+            except Exception, e:
+              	print "optional parameter not found"
+                N = 10
+          	matches = r.json()
+            matches = [x for x in matches if x['stationType'] == uri[0]] 
+            matches = [x for x in matches if int(x['bikes']) >= N]
+        	return json.dumps(matches)
+          
+		if uri[0] == 'count':
+            try:
+                district = params['district']
+               	matches = r.json()
+                matches = [x for x in matches if x['district'] == district]
+                bikes = sum([int(x['bikes']) for x in matches])
+                slots = sum([int(x['slots']) for x in matches])
+                matches = {'district': district, 'bikes': str(bikes), 'slots': str(slots)}
+            except:
+                raise cherrypy.HTTPError(404, "Invalid district")
+            return json.dump(matches)
 
 if __name__=='__main__':
     conf = {
@@ -38,6 +81,4 @@ if __name__=='__main__':
     cherrypy.config.update({'server.socket_port': 8080})
     cherrypy.engine.start()
     cherrypy.engine.block()
-
-
 
