f133bd0702d4d26e7d6df842ab43bcf2f33c14eb
Ninja <s223012@studenti.polito.it>
2018-12-15 23:52:52 +0100

    Example

---
+++ lab0/ThreadingExamples/example0.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import threading
+import time
+
+exitFlag = 0
+
+class myThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print "Starting " + self.name
+      print_time(self.name, self.counter, 1)
+      print "Exiting " + self.name
+
+def print_time(threadName, counter, delay):
+   while counter:
+      time.sleep(delay)
+      print "%s: %s" % (threadName, time.ctime(time.time()))
+      counter -= 1
+      #if exitFlag:
+      #	 threadName.exit()
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", 5)
+thread2 = myThread(2, "Thread-2", 5)
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+#time.sleep(3)
+#exitFlag = 1
+print "Exiting Main Thread"
---
+++ lab0/ThreadingExamples/example5_semaphore.py
@@ -0,0 +1,55 @@
+import threading
+import random
+import time
+    
+class SemaphoreThread( threading.Thread ):
+	"""Class using semaphores"""
+	# class variable
+	availableTables = [ "A", "B", "C", "D", "E" ]
+
+	def __init__( self, threadName, semaphore ):
+		"""Initialize thread using a semaphore variable and creates an attribute"""
+		threading.Thread.__init__( self, name = threadName )
+		self.sleepTime = random.randrange( 1, 6 )
+
+		# set the semaphore as a data attribute of the class
+		# all threads will shared the same semaphore
+		self.threadSemaphore = semaphore     
+
+	def run( self ):
+		"""Print message and release semaphore"""
+
+		# acquire the semaphore
+		self.threadSemaphore.acquire()
+		threadLock.acquire()
+		# remove a table from the list
+		table = SemaphoreThread.availableTables.pop()
+		print "%s entered; seated at table %s."  % ( self.getName(), table ), 
+		print SemaphoreThread.availableTables
+   		threadLock.release()
+		time.sleep( self.sleepTime )   # enjoy a meal
+
+		# free a table
+		threadLock.acquire()
+		print "   %s exiting; freeing table %s." % ( self.getName(), table ) 
+		SemaphoreThread.availableTables.append( table )
+		print SemaphoreThread.availableTables
+		threadLock.release()
+		# release the semaphore after execution finishes
+		self.threadSemaphore.release()
+   
+threadLock = threading.Lock()   
+threads = []  # list of threads
+  
+# semaphore allows five threads to enter critical section
+#treading.Semaphore is the semaphore method, returning a threadSemaphore variable
+threadSemaphore = threading.Semaphore(len( SemaphoreThread.availableTables ) )
+    
+# create ten threads
+for i in range( 1, 11 ):
+	threads.append( SemaphoreThread( "thread" + str( i ), threadSemaphore ) )
+  
+# start each thread
+for thread in threads:
+	thread.start()
+
